5d0266aeab7e28fec00940fe5bfd37c696b64cfd
ALUNO <ALUNO@10.85.198.5>
2024-06-04 16:04:25 -0300

    Commit project Chicken01

---
+++ src/Chickens01.java
@@ -0,0 +1,15 @@
+public class Chickens01 {
+    public static void main(String[] args) {
+       int count=15;
+       int a, b, c;
+       a = count++;
+       b = count + 4;
+       c = count - 6;
+       
+       //put your code here
+       
+       System.out.println("segunda feira = " + a + ",");
+       System.out.println("terca feira = " + b + ",");
+       System.out.println("quarta feira = " + c + ",");
+    }  // 3 = 15 | 4 = 20 | 2 = 10 
+}
